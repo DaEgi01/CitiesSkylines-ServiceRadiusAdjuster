5510f13d545c49aaa208ea83d36b7baeaab16b93
egi <daegi01@gmail.com>
2017-10-25 23:21:24 +0200

    increase version to 1.4.6

--- ServiceRadiusAdjuster/Properties/AssemblyInfo.cs
+++ ServiceRadiusAdjuster/Properties/AssemblyInfo.cs
@@ -13,4 +13,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("73d44027-0cf6-48d3-aaf7-7573a20a3a54")]
 
-[assembly: AssemblyVersion("1.4.5")]
+[assembly: AssemblyVersion("1.4.6")]
