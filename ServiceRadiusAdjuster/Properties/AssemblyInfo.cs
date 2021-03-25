eb4799a618da6f2e0c7cf32fe2f893b2e0999f4c
egi <daegi01@gmail.com>
2021-03-26 00:59:01 +0100

    moved to sdk style project

--- ServiceRadiusAdjuster/Properties/AssemblyInfo.cs
+++ ServiceRadiusAdjuster/Properties/AssemblyInfo.cs
@@ -1,16 +1,5 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Service Radius Adjuster")]
-[assembly: AssemblyDescription("A mod for 'Cities: Skylines' to adjust the service radius.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Egi Productions")]
-[assembly: AssemblyProduct("Service Radius Adjuster")]
-[assembly: AssemblyCopyright("MIT")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 [assembly: ComVisible(false)]
 [assembly: Guid("73d44027-0cf6-48d3-aaf7-7573a20a3a54")]
-
-[assembly: AssemblyVersion("1.5.*")]
