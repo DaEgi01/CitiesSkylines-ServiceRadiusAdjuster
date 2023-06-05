57cf8fc0f9b02013f87c204f924ccbfc1e454867
egi <daegi01@gmail.com>
2023-06-05 12:33:32 +0200

    small things for tests, nuget updates, cleanups etc.

--- ServiceRadiusAdjusterTests/DictionaryExtensionsTests.cs
+++ ServiceRadiusAdjusterTests/DictionaryExtensionsTests.cs
@@ -1,123 +1,123 @@
 ﻿using ServiceRadiusAdjuster;
 using System.Collections.Generic;
+using FluentAssertions;
 using Xunit;
 
-namespace ServiceRadiusAdjusterTests
+namespace ServiceRadiusAdjusterTests;
+
+public class DictionaryExtensionsTests
 {
-    public class DictionaryExtensionsTests
+    [Fact]
+    public void DictionaryCombine_New()
     {
-        [Fact(DisplayName = nameof(DictionaryCombine_New))]
-        public void DictionaryCombine_New()
+        var aDic = new Dictionary<string, float>()
         {
-            var aDic = new Dictionary<string, float>()
-            {
-                { "test1", 1.0f },
-                { "test2", 2.0f },
-                { "test3", 3.0f }
-            };
-            var bDic = new Dictionary<string, float>()
-            {
-                { "test4", 4.0f },
-                { "test5", 5.0f },
-                { "test6", 6.0f }
-
-            };
-            var expected = new Dictionary<string, float>()
-            {
-                { "test1", 1.0f },
-                { "test2", 2.0f },
-                { "test3", 3.0f },
-                { "test4", 4.0f },
-                { "test5", 5.0f },
-                { "test6", 6.0f }
-            };
-
-            var actual = aDic.CombineAndUpdate(bDic);
-
-            Assert.Equal(expected, actual);
-        }
-
-        [Fact(DisplayName = nameof(DictionaryCombine_Overlap))]
-        public void DictionaryCombine_Overlap()
+            { "test1", 1.0f },
+            { "test2", 2.0f },
+            { "test3", 3.0f }
+        };
+        var bDic = new Dictionary<string, float>()
         {
-            var aDic = new Dictionary<string, float>()
-            {
-                { "test1", 1.0f },
-                { "test2", 2.0f },
-                { "test3", 3.0f }
-            };
-            var bDic = new Dictionary<string, float>()
-            {
-                { "test1", 1.0f },
-                { "test2", 2.0f },
-                { "test3", 3.0f }
+            { "test4", 4.0f },
+            { "test5", 5.0f },
+            { "test6", 6.0f }
 
-            };
-            var expected = new Dictionary<string, float>()
-            {
-                { "test1", 1.0f },
-                { "test2", 2.0f },
-                { "test3", 3.0f }
-            };
-
-            var actual = aDic.CombineAndUpdate(bDic);
-
-            Assert.Equal(expected, actual);
-        }
-
-        [Fact(DisplayName = nameof(DictionaryCombine_UpdateOverlap))]
-        public void DictionaryCombine_UpdateOverlap()
+        };
+        var expected = new Dictionary<string, float>()
         {
-            var aDic = new Dictionary<string, float>()
-            {
-                { "test1", 1.0f },
-                { "test2", 2.0f },
-                { "test3", 3.0f }
-            };
-            var bDic = new Dictionary<string, float>()
-            {
-                { "test1", 5.0f },
-                { "test2", 6.0f },
-                { "test3", 7.0f }
-            };
-            var expected = new Dictionary<string, float>()
-            {
-                { "test1", 5.0f },
-                { "test2", 6.0f },
-                { "test3", 7.0f }
-            };
+            { "test1", 1.0f },
+            { "test2", 2.0f },
+            { "test3", 3.0f },
+            { "test4", 4.0f },
+            { "test5", 5.0f },
+            { "test6", 6.0f }
+        };
 
-            var actual = aDic.CombineAndUpdate(bDic);
+        var actual = aDic.CombineAndUpdate(bDic);
 
-            Assert.Equal(expected, actual);
-        }
+        actual.Should().BeEquivalentTo(expected);
+    }
 
-        [Fact(DisplayName = nameof(DictionaryCombine_New_Overlap_UpdateOverlap))]
-        public void DictionaryCombine_New_Overlap_UpdateOverlap()
+    [Fact]
+    public void DictionaryCombine_Overlap()
+    {
+        var aDic = new Dictionary<string, float>()
         {
-            var aDic = new Dictionary<string, float>()
-            {
-                { "test1", 1.0f },
-                { "test2", 2.0f },
-                { "test3", 3.0f }
-            };
-            var bDic = new Dictionary<string, float>()
-            {
-                { "test2", 2.0f },
-                { "test3", 4.0f },
-                { "test4", 5.0f }
-            };
-            var expected = new Dictionary<string, float>()
-            {
-                { "test1", 1.0f },
-                { "test2", 2.0f },
-                { "test3", 4.0f },
-                { "test4", 5.0f }
-            };
+            { "test1", 1.0f },
+            { "test2", 2.0f },
+            { "test3", 3.0f }
+        };
+        var bDic = new Dictionary<string, float>()
+        {
+            { "test1", 1.0f },
+            { "test2", 2.0f },
+            { "test3", 3.0f }
 
-            var actual = aDic.CombineAndUpdate(bDic);
+        };
+        var expected = new Dictionary<string, float>()
+        {
+            { "test1", 1.0f },
+            { "test2", 2.0f },
+            { "test3", 3.0f }
+        };
 
-            Assert.Equal(expected, actual);
-        }
+        var actual = aDic.CombineAndUpdate(bDic);
+
+        actual.Should().BeEquivalentTo(expected);
+    }
+
+    [Fact]
+    public void DictionaryCombine_UpdateOverlap()
+    {
+        var aDic = new Dictionary<string, float>()
+        {
+            { "test1", 1.0f },
+            { "test2", 2.0f },
+            { "test3", 3.0f }
+        };
+        var bDic = new Dictionary<string, float>()
+        {
+            { "test1", 5.0f },
+            { "test2", 6.0f },
+            { "test3", 7.0f }
+        };
+        var expected = new Dictionary<string, float>()
+        {
+            { "test1", 5.0f },
+            { "test2", 6.0f },
+            { "test3", 7.0f }
+        };
+
+        var actual = aDic.CombineAndUpdate(bDic);
+
+        actual.Should().BeEquivalentTo(expected);
+    }
+
+    [Fact]
+    public void DictionaryCombine_New_Overlap_UpdateOverlap()
+    {
+        var aDic = new Dictionary<string, float>()
+        {
+            { "test1", 1.0f },
+            { "test2", 2.0f },
+            { "test3", 3.0f }
+        };
+        var bDic = new Dictionary<string, float>()
+        {
+            { "test2", 2.0f },
+            { "test3", 4.0f },
+            { "test4", 5.0f }
+        };
+        var expected = new Dictionary<string, float>()
+        {
+            { "test1", 1.0f },
+            { "test2", 2.0f },
+            { "test3", 4.0f },
+            { "test4", 5.0f }
+        };
+
+        var actual = aDic.CombineAndUpdate(bDic);
+
+        actual.Should().BeEquivalentTo(expected);
     }
 }
--- ServiceRadiusAdjusterTests/ProfileTests.cs
+++ ServiceRadiusAdjusterTests/ProfileTests.cs
@@ -1,90 +1,67 @@
 ﻿using ServiceRadiusAdjuster.Model;
 using System.Collections.Generic;
 using System.Linq;
+using FluentAssertions;
 using Xunit;
 
-namespace ServiceRadiusAdjusterTests
+namespace ServiceRadiusAdjusterTests;
+
+public class ProfileTests
 {
-    public class ProfileTests
+    [Fact]
+    public void Combine_AddNewOptionItems()
     {
-        [Fact(DisplayName = nameof(Combine_AddNewOptionItems))]
-        public void Combine_AddNewOptionItems()
+        var existingViewGroups = new List<ViewGroup>()
         {
-            var existingOptionItems1 = new List<OptionItem>()
+            new ("vg1", 1, new List<OptionItem>()
             {
-                new OptionItem(ServiceType.Building, "oi1_1", "oi1_1", 10, 10, 100, 100),
-                new OptionItem(ServiceType.Building, "oi1_2", "oi1_2", 20, 20, 200, 200)
-            };
-
-            var existingOptionItems2 = new List<OptionItem>()
+                new (ServiceType.Building, "oi1_1", "oi1_1", 10, 10, 100, 100),
+                new (ServiceType.Building, "oi1_2", "oi1_2", 20, 20, 200, 200)
+            }),
+            new ("vg2", 2, new List<OptionItem>()
             {
-                new OptionItem(ServiceType.Building, "oi2_1", "oi2_1", 10, 10, 100, 100),
-                new OptionItem(ServiceType.Building, "oi2_2", "oi2_2", 20, 20, 200, 200)
-            };
+                new (ServiceType.Building, "oi2_1", "oi2_1", 10, 10, 100, 100),
+                new (ServiceType.Building, "oi2_2", "oi2_2", 20, 20, 200, 200)
+            })
+        };
 
-            var existingViewGroups = new List<ViewGroup>()
+        var profile = new Profile(existingViewGroups);
+
+        var newViewGroups = new List<ViewGroup>()
+        {
+            new ("vg1", 1, new List<OptionItem>()
             {
-                new ViewGroup("vg1", 1, existingOptionItems1),
-                new ViewGroup("vg2", 2, existingOptionItems2)
-            };
-
-            var profile = new Profile(existingViewGroups);
-
-            var newOptionItems1 = new List<OptionItem>()
+                new (ServiceType.Building, "oi1_1", "oi1_1", 10, 10, 100, 100),
+                new (ServiceType.Building, "oi1_2", "oi1_2", 20, 20, 200, 200),
+                new (ServiceType.Building, "oi1_3", "oi1_3", 30, 30, 300, 300)
+            }),
+            new ("vg2", 1, new List<OptionItem>()
             {
-                new OptionItem(ServiceType.Building, "oi1_1", "oi1_1", 10, 10, 100, 100),
-                new OptionItem(ServiceType.Building, "oi1_2", "oi1_2", 20, 20, 200, 200),
-                new OptionItem(ServiceType.Building, "oi1_3", "oi1_3", 30, 30, 300, 300)
-            };
+                new (ServiceType.Building, "oi2_1", "oi2_1", 10, 10, 100, 100),
+                new (ServiceType.Building, "oi2_2", "oi2_2", 20, 20, 200, 200),
+                new (ServiceType.Building, "oi2_3", "oi2_3", 30, 30, 300, 300)
+            })
+        };
 
-            var newOptionItems2 = new List<OptionItem>()
+        var expectedViewGroups = new List<ViewGroup>()
+        {
+            new ("vg1", 1, new List<OptionItem>()
             {
-                new OptionItem(ServiceType.Building, "oi2_1", "oi2_1", 10, 10, 100, 100),
-                new OptionItem(ServiceType.Building, "oi2_2", "oi2_2", 20, 20, 200, 200),
-                new OptionItem(ServiceType.Building, "oi2_3", "oi2_3", 30, 30, 300, 300)
-            };
-
-            var newViewGroups = new List<ViewGroup>()
+                new (ServiceType.Building, "oi1_1", "oi1_1", 10, 10, 100, 100),
+                new (ServiceType.Building, "oi1_2", "oi1_2", 20, 20, 200, 200),
+                new (ServiceType.Building, "oi1_3", "oi1_3", 30, 30, 300, 300)
+            }),
+            new ("vg2", 1, new List<OptionItem>()
             {
-                new ViewGroup("vg1", 1, newOptionItems1),
-                new ViewGroup("vg2", 1, newOptionItems2)
-            };
+                new (ServiceType.Building, "oi2_1", "oi2_1", 10, 10, 100, 100),
+                new (ServiceType.Building, "oi2_2", "oi2_2", 20, 20, 200, 200),
+                new (ServiceType.Building, "oi2_3", "oi2_3", 30, 30, 300, 300)
+            })
+        };
 
-            var expectedOptionItems1 = new List<OptionItem>()
-            {
-                new OptionItem(ServiceType.Building, "oi1_1", "oi1_1", 10, 10, 100, 100),
-                new OptionItem(ServiceType.Building, "oi1_2", "oi1_2", 20, 20, 200, 200),
-                new OptionItem(ServiceType.Building, "oi1_3", "oi1_3", 30, 30, 300, 300)
-            };
+        var expected = new Profile(expectedViewGroups).ViewGroups.SelectMany(vg => vg.OptionItems).ToList();
+        var actual = profile.Combine(newViewGroups).ViewGroups.SelectMany(vg => vg.OptionItems).ToList();
 
-            var expectedOptionItems2 = new List<OptionItem>()
-            {
-                new OptionItem(ServiceType.Building, "oi2_1", "oi2_1", 10, 10, 100, 100),
-                new OptionItem(ServiceType.Building, "oi2_2", "oi2_2", 20, 20, 200, 200),
-                new OptionItem(ServiceType.Building, "oi2_3", "oi2_3", 30, 30, 300, 300)
-            };
-
-            var expectedViewGroups = new List<ViewGroup>()
-            {
-                new ViewGroup("vg1", 1, newOptionItems1),
-                new ViewGroup("vg2", 1, newOptionItems2)
-            };
-
-            var expected = new Profile(expectedViewGroups).ViewGroups.SelectMany(vg => vg.OptionItems);
-            var actual = profile.Combine(newViewGroups).ViewGroups.SelectMany(vg => vg.OptionItems);
-
-            var expectedList = expected.ToList();
-            var actualList = actual.ToList();
-
-            for (int i = 0; i < expectedList.Count; i++)
-            {
-                var expectedItem = expectedList[i];
-                var actualItem = actualList[i];
-
-                Assert.Equal(expectedItem, actualItem);
-            }
-
-            Assert.Equal(expected, actual);
-        }
+        actual.Should().BeEquivalentTo(expected);
     }
 }
